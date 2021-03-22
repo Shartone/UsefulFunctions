410e6524a63db34510c95790ca8f1e918e992a39
Shartone <maxikabove@yandex.ru>
2021-03-22 13:45:52 +0300

    function

---
+++ document-depict.js
@@ -0,0 +1,11 @@
+}
+function show(id) {
+    document.getElementById(id).style.display = "inline";
+}
+function hide(id) {
+    document.getElementById(id).style.display = "none";
+}
+function write(text) {
+    document.getElementById(/тут пишется id элемента, который нужно прописать*/).innerHTML = text;
+}
+
